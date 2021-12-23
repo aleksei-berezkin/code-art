963747df49239f1dfc89601e9bf24454dab6174c
Alexey Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2021-12-23 19:20:40 +0300

    Flat loop

--- src/createGrid.ts
+++ src/createGrid.ts
@@ -15,48 +15,58 @@ export function createGrid(xMin: number, yMin: number,
     const avgW = lRasters.reduce((sum, r) => sum + r.w, 0) / lRasters.length;
     const maxAscent = lRasters.reduce((max, r) => r.ascent > max ? r.ascent : max, 0);
 
-    const lines = source.split(/[\r\n]+/);
-
     const _xMin = Math.floor(xMin / avgW) * avgW;
     const _yMin = Math.floor(yMin / fontSize) * fontSize;
-    for (let i = 0, y = _yMin;
-         i < lines.length && y <= yMax;
-         y += fontSize, i++
-    ) {
-        const baseline = y + maxAscent;
-        const line = lines[i];
-        for (let j = 0, x = _xMin;
-             j < line.length && x <= xMax;
-             j++
-        ) {
-            const letter = line[j];
-            const r = lettersMap.get(letter)!;
-            const rectVertices = rect3dConstZ(
-                x, baseline - r.ascent / fontSizeMultiplier,
-                x + r.w / fontSizeMultiplier, baseline + r.descent / fontSizeMultiplier,
-                z,
-            )
-            vertices.push(...rectVertices);
 
-            texPosition.push(...rect2d(
-                r.x, r.baseline - r.ascent,
-                r.x + r.w, r.baseline + r.descent,
-            ));
-
-            const color = !!/[$_a-zA-Z]/.exec(letter) ? [1, .8, .9, 1]
-                : !!/[0-9]/.exec(letter) ? [.8, .8, 1, 1]
-                : !!/["']/.exec(letter) ? [.8, 1, .7, 1]
-                : [1, 1, 1, 1];
-            const verticesNum = rectVertices.length / vertexSize3d;
-            colors.push(
-                ...Array.from({length: verticesNum})
-                    .flatMap(() => color)
-            );
-
-            x += r.w / fontSizeMultiplier;
+    let x = _xMin;
+    let y = _yMin;
+    for (let i = 0; i < source.length; i++) {
+        let letter = source[i];
+        if (letter === '\n') {
+            x = _xMin;
+            y += fontSize;
+            if (y > yMax) {
+                break;
+            }
+            continue;
         }
+
+        if (letter.charCodeAt(0) < 32) {
+            if (letter === '\t') {
+                letter = ' ';
+            } else {
+                continue;
+            }
+        }
+
+        const baseline = y + maxAscent;
+        const r = lettersMap.get(letter)!;
+
+        const rectVertices = rect3dConstZ(
+            x, baseline - r.ascent / fontSizeMultiplier,
+            x + r.w / fontSizeMultiplier, baseline + r.descent / fontSizeMultiplier,
+            z,
+        )
+        vertices.push(...rectVertices);
+
+        texPosition.push(...rect2d(
+            r.x, r.baseline - r.ascent,
+            r.x + r.w, r.baseline + r.descent,
+        ));
+
+        const color = !!/[$_a-zA-Z]/.exec(letter) ? [1, .8, .9, 1]
+            : !!/[0-9]/.exec(letter) ? [.8, .8, 1, 1]
+            : !!/["']/.exec(letter) ? [.8, 1, .7, 1]
+            : [1, 1, 1, 1];
+        const verticesNum = rectVertices.length / vertexSize3d;
+        colors.push(
+            ...Array.from({length: verticesNum})
+                .flatMap(() => color)
+        );
+
+        x += r.w / fontSizeMultiplier;
     }
-    console.log(texPosition);
+
     return {vertices, texPosition, colors};
 }
 
