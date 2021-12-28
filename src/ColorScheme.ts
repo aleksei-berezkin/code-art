d11585fc61c1fde1d5e1cdc7d5a0f8f1730527ef
Alexey Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2021-12-28 21:04:29 +0300

    More color schemes

--- src/ColorScheme.ts
+++ src/ColorScheme.ts
@@ -15,7 +15,7 @@ export type ColorScheme = {
 }
 
 
-export type ColorSchemeName = 'VS Code Dark+' | 'IntelliJ Darkula';
+export type ColorSchemeName = 'VS Code Dark+' | 'IntelliJ Darkula' | 'Chrome Dark' | 'Sahara' | 'Ocean' | 'Rainbow';
 
 export const colorSchemes: {[k in ColorSchemeName]: ColorScheme} = {
     'VS Code Dark+': {
@@ -39,5 +39,49 @@ export const colorSchemes: {[k in ColorSchemeName]: ColorScheme} = {
         keyword1: hexToRgba('#cc7832'),
         keyword2: hexToRgba('#cc7832'),
         default: hexToRgba('#a9b7c6'),
+    },
+    'Chrome Dark': {
+        background: hexToRgba('#202124'),
+        comment: hexToRgba('#747474'),
+        number: hexToRgba('#a1f7ea'),   // Changed
+        string: hexToRgba('#f28b54'),
+        name: hexToRgba('#5db0d7'),
+        member: hexToRgba('#d2c057'),
+        keyword1: hexToRgba('#9ef2b2'),
+        keyword2: hexToRgba('#9a7fd5'),
+        default: hexToRgba('#e8eaed'),
+    },
+    'Sahara': {
+        background: hexToRgba('#202020'),
+        comment: hexToRgba('#797056'),
+        number: hexToRgba('#92d277'),
+        string: hexToRgba('#b2b2b2'),
+        name: hexToRgba('#eacc66'),
+        member: hexToRgba('#eacc66'),
+        keyword1: hexToRgba('#f56545'),
+        keyword2: hexToRgba('#f56545'),
+        default: hexToRgba('#c5cee1'),
+    },
+    'Ocean': {
+        background: hexToRgba('#102820'),
+        comment: hexToRgba('#78889d'),
+        number: hexToRgba('#77d2a5'),
+        string: hexToRgba('#96ab93'),
+        name: hexToRgba('#66a8ea'),
+        member: hexToRgba('#59d2d2'),
+        keyword1: hexToRgba('#4f5bc2'),
+        keyword2: hexToRgba('#8e4cd0'),
+        default: hexToRgba('#bbb3ad'),
+    },
+    'Rainbow': {
+        background: hexToRgba('#181818'),
+        comment: hexToRgba('#bdbdbd'),
+        number: hexToRgba('#ff8c16'),
+        string: hexToRgba('#f1d230'),
+        name: hexToRgba('#7485ef'),
+        member: hexToRgba('#5fc9d9'),
+        keyword1: hexToRgba('#3bbd1a'),
+        keyword2: hexToRgba('#bd1537'),
+        default: hexToRgba('#a9a9a9'),
     },
 }
