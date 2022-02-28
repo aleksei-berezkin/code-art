1e932decd7d112fe124782911928a71ec6ea4fa0
Alexey Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2022-02-28 20:28:21 +0300

    impliedHeight for moderate angles

--- src/model/PixelSpace.ts
+++ src/model/PixelSpace.ts
@@ -3,6 +3,9 @@ import { degToRad } from '../util/degToRad';
 
 export type PixelSpace = ReturnType<typeof makePixelSpace>;
 
+const viewAngleV = degToRad(115);
+const impliedAspectRatio = 1.9;
+
 /**
  * See figures/01_PixelSpace.png
  *
@@ -10,18 +13,21 @@ export type PixelSpace = ReturnType<typeof makePixelSpace>;
  * Z goes from -zBase to +large_val*zBase where z=-zBase is a camera position,
  * and z=0 is a distance at which an object with height=h is fully visible.
  *
+ * impliedHeight is a height of image with the same width which would
+ * perfectly fit impliedAspectRatio.
+ * 
  * Vertical view angle is 115 deg (like that of a human eye) so the following
  * equation is valid:
  * 
- * tan(155deg / 2) = (h/2) / zBase
+ * tan(155deg / 2) = (naturalHeight/2) / zBase
  *
  * An object at z=+zBase (2*zBase distance from the camera) is twice smaller than that
  * at z=0, so w = (zBase + z) / zBase = 1 + z / zBase
  */
 export function makePixelSpace(size: Size) {
     const {w, h} = size;
-    const viewAngleV = degToRad(115);
-    const zBase = h / 2 / Math.tan(viewAngleV / 2);
+    const impliedHeight = Math.max(h, w / impliedAspectRatio);
+    const zBase = Math.max(w, impliedHeight) / 2 / Math.tan(viewAngleV / 2);
     const viewAngleH = Math.atan(w / 2 / zBase) * 2;
     const zMin = -zBase;
     const zMax = 1000 * zBase;
